c0d0ea06614b8816e8cb338e8cbb9a084274fdde
Бугаков А. И <BugakovAI@elcom.local>
2020-03-17 18:28:29 +0700

    Вынес логику в методы класса ComControl

--- Classes.cs
+++ Classes.cs
@@ -9,12 +9,14 @@ namespace ComPort
     public class ComControl
     {
         SerialPort port = new SerialPort(); //объявили порт
+        string portName;
+        public ComControl(string portName) {this.portName = portName;}
 
-        public void Open(string name) //метод открытия порта
+        public void Open() //метод открытия порта
         {
             try
             {
-                port.PortName = name;
+                port.PortName = portName;
                 port.BaudRate = 9600;
                 port.DataBits = 8;
                 port.Parity = Parity.Odd;
@@ -25,7 +27,7 @@ namespace ComPort
                     port.Close();
                 port.Open();
                 if (port.IsOpen == true) 
-                    Console.WriteLine($"Порт {port.PortName} открылся!");
+                    Console.WriteLine($"Порт {port.PortName} открыт!");
             }
             catch (System.Exception e )
             {
@@ -40,11 +42,55 @@ namespace ComPort
             if (port.IsOpen == false) 
                 Console.WriteLine($"\nПорт {port.PortName} закрылся!");
         }
+        
+        public bool OpenChannel(byte id)  //метод записи в порт
+        {
+            byte[] req = new byte[8] {id, 0x01, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30};//запрос на открытие канала связи, где 1ый байт - код функции, 2-7 байты - пароль "000000" в ASCII, 8,9 - CRC.
+            byte[] crc = CRC16Calc(req);            //получаем контрольную суммму для запроса 
+            byte[] send = new byte[req.Length + 2]; //cоздаём массив для запроса + CRC 
+            Array.Copy(req, 0, send, 0, req.Length);//копируем из req с 0-ого индекса в send начиная с 0-ого индекса, 8 элементов.
+            Array.Copy(crc, 0, send, req.Length, 2);//копируем из crc с 0-ого индекса в send начиная с 8-ого индекса, 2 элемента.
+            byte[] forcrc = new byte[] {id, 0x00};  //тело ответа для вычисления корректного CRC
+            crc = CRC16Calc(forcrc);                //считаем корректный CRC ответа  
+            try
+            {
+                Write(send);                        //отправляем запрос на открытие канала + CRC в порт.
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine($"ERROR: невозможно произвести запись в порт: {e}");
+            }
+            System.Threading.Thread.Sleep(50);      //делаем паузу в 50 мс
+            byte[] answ = Read(id);                   //читаем порт
+            //Проверяем корректность ответа счётчика
+            if (answ[0] == id && answ[1] == 0x00 && answ[2] == crc[0] && answ[3] == crc[1])
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public byte[] GetReq(byte id)  //метод формирования запроса
+        {
+            byte req = 0x08;        // код запроса 08 - чтение праметров и данных 
+            byte param = 0x1B;      // код параметра 1В - чтение данных в формате float
+            byte dataArray = 0x00;  // код массива данных 00 - данные вспомогательных режимов измерения (RWRI) (по таблице 2-45)
+            byte rwri = 0x11;       // код вспомогательного режима измерения (RWRI) 11 - напряжение фазное по фазе 1 (по таблице 2-38)
+            
+            byte[] body = new byte[5] {id, req, param, dataArray, rwri};    //запрос на чтение данных
+            byte[] crc = CRC16Calc(body);                                   //получаем контрольную суммму для запроса 
+            byte[] request = new byte[body.Length + crc.Length];            //cоздаём массив для запроса + CRC 
+            Array.Copy(body, 0, request, 0, body.Length);                   //копируем из req в send
+            Array.Copy(crc, 0, request, body.Length, 2);                    //копируем из crc в send
+            return request;
+        }
+
         public void Write(byte[] data)  //метод записи в порт
         {
             try
             {
                 port.Write (data, 0, data.Length);
+                //Console.WriteLine("Запрос записан в порт");
             }
             catch (System.Exception e)
             {
@@ -53,42 +99,31 @@ namespace ComPort
             }
         }
         
-        public byte[] Read()  //метод чтения из порта
-        {
+        public byte[] Read(byte id)  //метод чтения из порта
+        {   
+            System.Threading.Thread.Sleep(50); 
+            byte[] fail = new byte[3] {0,0,0};
             if ((int)port.BytesToRead > 0)
             {
                 byte[] answer = new byte[(int)port.BytesToRead];
                 port.Read(answer, 0, port.BytesToRead);
-                return answer;
+                //Вычисляем CRC, для проверки корректности ответа счётчика
+                byte[] forcrc = new byte[answer.Length-2];                  // массив для расчёта CRC длиной равной длине тела ответа
+                Array.Copy(answer, 0, forcrc, 0, forcrc.Length);            // копируем только тело ответа в forcrcData без CRC
+                byte[] crc = CRC16Calc(forcrc);                             // считаем CRC ответа          
+                // Проверяем корректность ответа счётчика
+                if (answer[0] == id && answer[(answer.Length)-2] == crc[0] && answer[(answer.Length)-1] == crc[1])
+                {
+                    return answer;
+                }
+                return fail;
             }
             else
-            {
-                byte[] fail = new byte[] {0,0,0};
+            {                
                 return fail;
             }
 
         }
-
-        /*
-        public void Write(string data)  //метод записи в порт
-        {
-            byte[] message = new byte[data.Length];
-            message = Encoding.Default.GetBytes(data);
-            port.Write (message, 0, message.Length);
-            //System.Threading.Thread.Sleep(2000);
-        }
-        
-        public string Read()  //метод чтения из порта
-        {
-            if (port.BytesToRead > 0)
-            {
-                byte[] answer = new byte[(int)port.BytesToRead];
-                port.Read(answer, 0, port.BytesToRead);
-                return Encoding.Default.GetString(answer);
-            }
-            return "0";
-        }
-        */
 
         public byte[] CRC16Calc(byte[] Message)
         {
--- Program.cs
+++ Program.cs
@@ -9,138 +9,39 @@ namespace ComPort
     {
         static void Main(string[] args)
         {
-            /*
-            ComControl COM = new ComControl();
-            ComControl COM2 = new ComControl();
+            // 0. Данные соединения
+            string comNum = "COM8"; //Номер COM порта, к которму подключен счётчик
+            byte id = 0x13;         //сетевой адрес счётчика          
             
-            COM.Open("COM10");
-            COM2.Open("COM11");
-            
-            COM.Write("порт COM10 передаёт привет порту COM11");
-            string answ = COM2.Read();
-
-            Console.WriteLine(answ);
-
-            COM.Close();
-            COM2.Close();
-            */
-
             // 1. Открываем последовательный порт 
-
-            ComControl COM = new ComControl();
-            COM.Open("COM8");
-
+            ComControl COM8 = new ComControl(comNum);
+            COM8.Open();
+            
             // 2. Открываем канал связи счётчика
-
-            byte id = 0x13;                                                         //адрес счётчика
-            byte[] req = new byte[8] {id, 0x01, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30};//запрос на открытие канала связи, где 1ый байт - код функции, 2-7 байты - пароль "000000" в ASCII, 8,9 - CRC.
-            byte[] crc = COM.CRC16Calc(req);                                        //получаем контрольную суммму для запроса 
-            byte[] send = new byte[req.Length + crc.Length];                        //cоздаём массив для запроса+CRC 
-            
-            Array.Copy(req, 0, send, 0, req.Length);    //копируем из req с 0-ого индекса в send начиная с 0-ого индекса, 8 элементов.
-            Array.Copy(crc, 0, send, req.Length, 2);    //копируем из crc с 0-ого индекса в send начиная с 8-ого индекса, 2 элемента.
-            
-            COM.Write(send);                            //отправляем запрос на открытие канала + CRC в порт.
-            
-            //выводим в консоль запрос который отправили в порт
-            Console.WriteLine("Запрос на открытие канала (hex): ");
-            foreach (byte i in send)
+            bool isOpen = COM8.OpenChannel(id);
+            if (isOpen==true) 
             {
-                Console.Write(i.ToString("X")); 
-            }
-            System.Threading.Thread.Sleep(1000);        //делаем паузу в 1 сек
-
+                Console.WriteLine("Канал открыт!");
             
-            // 3. Вычисляем CRC, для проверки корректности ответа счётчика
+                // 3. Формируем запрос на чтение данных и отправляем его в порт
+                byte[] request = COM8.GetReq(id);
+                COM8.Write(request);
 
-            byte[] forcrc = new byte[] {id, 0x00};      //тело ответа для вычисления CRC
-            crc = COM.CRC16Calc(forcrc);                //считаем CRC ответа          
-            Console.WriteLine("\nРасчётная контрольная сумма ответа счётчика: ");
-            foreach (byte i in crc)
-            {
-                Console.Write(i.ToString("X"));
+                // 4. Теперь читаем ответ из порта
+                byte[] response = COM8.Read(id);
+
+                // 5. Выводим значение в консоль
+                byte[] byteValue = new byte[4]; // массив для значения параметра
+                Array.Copy(response, 1, byteValue, 0, 4);
+                float value = BitConverter.ToSingle(byteValue,0);
+                Console.WriteLine($"Текущее значение напряжения на фазе А = {value}");
+
+                Console.WriteLine("Для выхода нажмите любую клавишу ...");
+                Console.ReadKey();
             }
-            
-            // 4. Читаем ответ счётчика с порта
+            else {Console.WriteLine("Канал не открыт...");}
 
-            byte[] answ = COM.Read();                   //читаем порт
-            Console.WriteLine("\nОтвет счётчика на запрос: ");
-            foreach (byte i in answ)
-            {
-                Console.Write(i.ToString("X"));
-            }
-            
-            // 5. Проверяем корректность ответа счётчика
-
-            if (answ[0] == id && answ[1] == 0x00 && answ[2] == crc[0] && answ[3] == crc[1])
-            {
-                Console.WriteLine("\nПоздравляю, канал связи счётчика открыт!");
-            }
-            else {Console.WriteLine("\nНе удалось открыть канал связи...");}
-
-
-            // 6. Формируем запрос на чтение данных и отправляем его в порт
-            byte reqCode = 0x08;        // код запроса 08 - чтение праметров и данных 
-            byte paramCode = 0x1B;      // код параметра 1В - чтение данных в формате float
-            byte dataArrayCode = 0x00;  // код массива данных 00 - данные вспомогательных режимов измерения (RWRI) (по таблице 2-45)
-            byte rwri = 0x11;             // код вспомогательного режима измерения (RWRI) 11 - напряжение фазное по фазе 1 (по таблице 2-38)
-
-            byte[] reqData = new byte[5] {id, reqCode, paramCode, dataArrayCode, rwri};     //запрос на чтение данных
-            byte[] crcData = COM.CRC16Calc(reqData);                                        //получаем контрольную суммму для запроса 
-            byte[] sendData = new byte[reqData.Length + crcData.Length];                    //cоздаём массив для запроса + CRC 
-            
-            Array.Copy(reqData, 0, sendData, 0, reqData.Length);                 //копируем из req в send
-            Array.Copy(crcData, 0, sendData, reqData.Length, crcData.Length);    //копируем из crc в send
-            System.Threading.Thread.Sleep(1000);        //делаем паузу в 1 сек
-            COM.Write(sendData);        //отправляем запрос на открытие канала + CRC в порт.
-            
-            //выводим в консоль запрос который отправили в порт
-            Console.WriteLine("Запрос на чтение U фазы 1 (hex): ");
-            foreach (byte i in sendData)
-            {
-                Console.Write(i.ToString("X")); 
-            }
-
-            // 7. Читаем ответ счётчика с порта
-            System.Threading.Thread.Sleep(1000);        //делаем паузу в 1 сек
-            byte[] answData = COM.Read();   //читаем порт
-            Console.WriteLine("\nОтвет счётчика на запрос: ");
-            foreach (byte i in answData)
-            {
-                Console.Write(i.ToString("X"));
-            }
-            
-
-            // 8. Вычисляем CRC, для проверки корректности ответа счётчика
-            byte[] forcrcData = new byte[answData.Length-2];            // массив для расчёта CRC длиной равной длине тела ответа
-            Array.Copy(answData, 0, forcrcData, 0, forcrcData.Length);  // копируем только тело ответа в forcrcData без CRC
-            crcData = COM.CRC16Calc(forcrcData);                        //считаем CRC ответа          
-            Console.WriteLine("\nРасчётная контрольная сумма: ");
-            foreach (byte i in crcData)
-            {
-                Console.Write(i.ToString("X"));
-            }
-            
-            // 9. Проверяем корректность ответа счётчика
-
-            if (answData[0] == id && answData[5] == crcData[0] && answData[6] == crcData[1])
-            {
-                Console.WriteLine("\nПоздравляю, ответ корректен!");
-            }
-            else {Console.WriteLine("\nУвы, ответ не корректен!...");}
-
-            // 10. Выводим значение в консоль
-
-            byte[] byteValue = new byte[4];            // массив для значения парамтера
-            Array.Copy(answData, 1, byteValue, 0, byteValue.Length);
-            float value = BitConverter.ToSingle(byteValue,0);
-            Console.WriteLine($"\nТекущее значение напряжения на фазе А = {value}");
-
-            Console.WriteLine("Для выхода нажмите любую клавишу ...");
-            Console.ReadKey();
-
-            COM.Close();
-            
+            COM8.Close();
         }
     }
 }
